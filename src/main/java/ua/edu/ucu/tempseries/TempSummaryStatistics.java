e4c3fd7d1c8eac588c367fd015c94d7d56b63af3
abalakirskaya <45092662+abalakirskaya@users.noreply.github.com>
2019-10-18 19:47:43 +0300

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,5 +1,32 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    
+    public double average_temp;
+    public double deviation_temp;
+    public double min_temp;
+    public double max_temp;
+
+    public TempSummaryStatistics(double average_temp, double deviation_temp, double min_temp, double max_temp){
+        this.average_temp = average_temp;
+        this.deviation_temp = deviation_temp;
+        this.min_temp = min_temp;
+        this.max_temp = max_temp;
+    }
+
+    public double get_average_temp(){
+        return average_temp;
+    }
+
+    public double get_deviation_temp(){
+        return deviation_temp;
+    }
+
+    public double get_min_temp(){
+        return min_temp;
+    }
+
+    public double get_max_temp(){
+        return max_temp;
+    }
+
 }
